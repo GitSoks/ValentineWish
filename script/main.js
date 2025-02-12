74b6ba04bc737f6a360395384300f2a3290db9a1
David <35436218+GitSoks@users.noreply.github.com>
2025-02-12 12:40:18 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -266,7 +266,7 @@ const animationTimeline = () => {
     );
 
   // tl.seek("currentStep");
-  // tl.timeScale(2);
+  tl.timeScale(2);
 
   // Restart Animation on click
   const replyBtn = document.getElementById("replay");
